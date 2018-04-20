adf0c723716791887f81d1d2b85e4fa8b5ad3fed
selvajitheone <selvajitheone@gmail.com>
2018-04-20 10:19:27 +0530

    -v

---
+++ git-push.ps1
@@ -0,0 +1,13 @@
+﻿
+#Auto git add, commit and push
+#Author :  Selva
+
+cd "D:\Tutorials\Interview preparion\Azure-DevOps-KB"
+
+[var]$username = selvajitheone
+[var]$password = google@1
+
+git add . 
+git commit -m "$string"
+git push
+
